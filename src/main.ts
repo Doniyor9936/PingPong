7a46019344b543b7afefa79c5f007ea174730fdf
Doniyor Qalandarov <doniyorqalandarov853@gmail.com>
2025-05-29 13:43:28 +0500

    last changes

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Ping Pong example')
+    .setDescription('The ping pong API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('ping pong')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
